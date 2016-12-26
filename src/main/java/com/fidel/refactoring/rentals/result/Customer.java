a4d2c343f4caf4de46ce72f3b1d0980a980bc63b
Andrii Mykytchyn <andrii.mykytchyn@bazaarvoice.com>
2016-12-26 16:48:16 +0200

    Extract Method, Replace Temp with Query

--- src/main/java/com/fidel/refactoring/rentals/result/Customer.java
+++ src/main/java/com/fidel/refactoring/rentals/result/Customer.java
@@ -1,9 +1,7 @@
 package com.fidel.refactoring.rentals.result;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Vector;
 
 public class Customer {
     private String _name;
@@ -22,52 +20,48 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Учет аренды для " + getName() + "\n";
         for (Rental _rental : _rentals) {
-            //определить сумму для каждой строки
-            double each = amountFor(_rental);
-
-            // добавить очки для активного арендатора
-            frequentRenterPoints++;
-            // бонус за аренду новинки на два дня
-            if ((_rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    _rental.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
-            //показать результаты для этой аренды
-
-            result += "\t" + _rental.getMovie().getTitle() + "\t" +
-                    String.valueOf(each) + "\n";
-            totalAmount += each;
+            // показать результаты по каждой аренде
+            result += _rental.getMovie().getTitle() + " " +
+                    String.valueOf(_rental.getCharge()) + "\n";
         }
         //добавить нижний колонтитул
         result += "Сумма задолженности составляет " +
-                String.valueOf(totalAmount) + "\n";
-        result += "Вы заработали " + String.valueOf(frequentRenterPoints) +
+                String.valueOf(getTotalCharge()) + "\n";
+        result += "Вы заработали " + String.valueOf(getTotalFrequentRenterPoints()) +
                 " очков за активность";
         return result;
     }
 
-    private double amountFor(Rental each) { double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 15;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 15;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 15;
-                }
-                break;
+    public String htmlStatement() {
+        String result = "<Н1>0перации аренды для <ЕМ>" + getName() + "</EM></H1><P>\n";
+        for (Rental _rental : _rentals) {
+            // показать результаты по каждой аренде
+            result += _rental.getMovie().getTitle() + " " +
+                    String.valueOf(_rental.getCharge()) + "<BR>\n";
         }
-        return thisAmount;
+        //добавить нижний колонтитул
+        result += "<Р>Ваша задолженность составляет <ЕМ>" +
+                String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "На этой аренде вы заработали <ЕМ>" +
+                String.valueOf(getTotalFrequentRenterPoints()) + "</ЕМ> очков за активность<Р>";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        for (Rental _rental : _rentals) {
+            result += _rental.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        for (Rental _rental : _rentals) {
+            result += _rental.getFrequentRenterPoints();
+        }
+        return result;
     }
 }
--- src/main/java/com/fidel/refactoring/rentals/result/Rental.java
+++ src/main/java/com/fidel/refactoring/rentals/result/Rental.java
@@ -16,4 +16,36 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2) {
+                    result += (getDaysRented() - 2) * 15;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 15;
+                if (getDaysRented() > 3) {
+                    result += (getDaysRented() - 3) * 15;
+                }
+                break;
+        }
+        //System.out.println("\t" + getMovie().getTitle() + " " + result);
+        return result;
+    }
+
+    int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                getDaysRented() > 1) {
+            return 2;
+        }
+        //показать результаты для этой аренды
+        return 1;
+    }
 }
